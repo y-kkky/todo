6f251a00d4aa2de8a3f7575812b3e8e3199aae8d
yar1k <yarik.just@gmail.com>
2013-06-10 20:53:25 +0300

    Time

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,8 +10,6 @@ import java.text.SimpleDateFormat
 
 object Application extends Controller {
 
-
-
   def index = Action {
     Redirect(routes.Application.tasks)
   }
@@ -23,17 +21,14 @@ object Application extends Controller {
   def newTask = Action { implicit request =>
     taskForm.bindFromRequest.fold(
       errors => BadRequest(views.html.index(Task.all(), errors)),
-      x=>x match { case(label,who) => {
-        // Получаем текущее время
-        val today = Calendar.getInstance().getTime()
-        val timeFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
-        val time = timeFormat.format(today)
-        //----------------------------
-        Task.create(label, who, time)
-        Redirect(routes.Application.tasks)
+      x => x match {
+        case (label, who) => {
+          // Получаем текущее время
+          //----------------------------
+          Task.create(label, who, getTime())
+          Redirect(routes.Application.tasks)
         }
-      }
-    )
+      })
   }
 
   def deleteTask(id: Long) = Action {
@@ -47,10 +42,15 @@ object Application extends Controller {
   }
 
   val taskForm = Form(
-    tuple (
+    tuple(
       "label" -> nonEmptyText,
-      "who" -> nonEmptyText
-    )
-  )
+      "who" -> nonEmptyText))
+  private def getTime(): String = {
+    val today = Calendar.getInstance().getTimeInMillis()
+    val slow = Calend.ge
+    val timeFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss")
+    val time = timeFormat.format(today)
+    time
+  }
 
 }
